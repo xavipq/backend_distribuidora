f63f63098bf323f193cccf2e00d131e00379560b
xavipq <xavieraperez23@gmail.com>
2025-03-20 21:38:34 -0600

    Subiendo proyecto descargado

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'mysql2051';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'xavi';
 export const DB_DATABASE = process.env.DB_DATABASE || 'distribuidora_bd';
 
 //export const PORT = process.env.PORT;
